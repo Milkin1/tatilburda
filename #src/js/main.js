aa484f4de9ff6b4bc7545cec0900cc773eeceb74
Milkin1 <mammadov.ilkin@hotmail.com>
2021-12-15 01:05:55 +0300

    navigation menu was made

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,13 +1,40 @@
+//burger menu
 const burger = document.querySelector('.main__burger'),
-    navigation = document.querySelector('.main-navigation')
+    navigation = document.querySelector('.main-navigation'),
+    burgerBtn = document.querySelector('.burger-name'),
+    logo = document.querySelector('.sm-logo');
 
 burger.addEventListener('click', (e) => {
     if (e.target.classList.contains('active')) {
         e.currentTarget.classList.remove('active');
         navigation.classList.remove('active');
+        burgerBtn.classList.remove('active');
+        logo.classList.remove('active');
     } else {
         e.currentTarget.classList.add('active');
         navigation.classList.add('active');
+        burgerBtn.classList.add('active');
+        logo.classList.add('active');
     }
-
 });
+
+
+//tabs
+const tabActivators = document.querySelectorAll(".tab_activator");
+const tabChilds = document.querySelectorAll(".tab_child");
+
+tabActivators.forEach((tabActivator) => {
+    tabActivator.addEventListener('click', function (e) {
+        tabActivators.forEach((tabActivator2) => {
+            tabActivator2.classList.remove('active')
+        })
+        e.target.classList.add('active')
+        tabChilds.forEach((tabChild) => {
+            if (e.target.dataset.tab == tabChild.dataset.tab) {
+                tabChild.classList.add('active')
+            } else {
+                tabChild.classList.remove('active')
+            }
+        })
+    })
+})
