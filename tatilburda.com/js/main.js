6d3ef20ada125c842e6bfadec01c807a0b29dba3
Milkin1 <mammadov.ilkin@hotmail.com>
2021-12-16 01:49:30 +0300

    navigation and dropdown

--- tatilburda.com/js/main.js
+++ tatilburda.com/js/main.js
@@ -5,24 +5,25 @@ const burger = document.querySelector('.main__burger'),
     logo = document.querySelector('.sm-logo');
 
 burger.addEventListener('click', (e) => {
-    if (e.target.classList.contains('active')) {
-        e.currentTarget.classList.remove('active');
-        navigation.classList.remove('active');
-        burgerBtn.classList.remove('active');
-        logo.classList.remove('active');
-    } else {
-        e.currentTarget.classList.add('active');
-        navigation.classList.add('active');
-        burgerBtn.classList.add('active');
-        logo.classList.add('active');
+        if (e.target.classList.contains('active')) {
+            e.currentTarget.classList.remove('active');
+            navigation.classList.remove('active');
+            burgerBtn.classList.remove('active');
+            logo.classList.remove('active');
+        } else {
+            e.currentTarget.classList.add('active');
+            navigation.classList.add('active');
+            burgerBtn.classList.add('active');
+            logo.classList.add('active');
+        }
     }
-});
+
+);
 
 
 //tabs
 const tabActivators = document.querySelectorAll(".tab_activator");
 const tabChilds = document.querySelectorAll(".tab_child");
-
 tabActivators.forEach((tabActivator) => {
     tabActivator.addEventListener('click', function (e) {
         tabActivators.forEach((tabActivator2) => {
@@ -38,3 +39,37 @@ tabActivators.forEach((tabActivator) => {
         })
     })
 })
+
+//burger dropdown
+const hotels = document.querySelector('.hotel__dropdown'),
+    contact = document.querySelector('.hotel__contact');
+
+hotels.addEventListener('click', (e) => {
+    if (contact.classList.contains('active')) {
+        contact.classList.remove('active');
+    } else {
+        contact.classList.add('active');
+    }
+});
+
+//burger menu hide
+const burgerNavigation = document.querySelector('.main-navigation'),
+    navigationList = document.querySelector(".main-navigation__list");
+let viewportWidth = window.innerWidth;
+console.log(viewportWidth);
+
+
+window.addEventListener('resize', () => {
+    viewportWidth = window.innerWidth;
+    viewChange();
+});
+
+viewChange();
+
+function viewChange() {
+    if (viewportWidth >= 1024) {
+        burgerNavigation.classList.add('hide');
+    } else {
+        burgerNavigation.classList.remove('hide');
+    }
+}
